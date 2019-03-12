09603b77316fd356a6baba24ac7ecb8b89f7cec0
Ferico Samuel <ferico55@gmail.com>
2019-03-12 16:26:25 +0700

    initial commit

---
+++ Texture-Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Texture-Tutorial
+//
+//  Created by Ferico Samuel on 12/03/19.
+//  Copyright © 2019 Tokopedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
